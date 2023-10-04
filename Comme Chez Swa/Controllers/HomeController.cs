db24c9fec9472543f0b3b09af26a6a3cc383c2a2
Tibo Van Gindertaelen <tibo.vg@gmail.com>
2023-10-04 11:57:00 +0200

    Projects initialised

---
+++ Comme Chez Swa/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Comme_Chez_Swa.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Comme_Chez_Swa.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
